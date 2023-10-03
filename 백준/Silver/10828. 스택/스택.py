870829e6f15099df8e0389ceda306fb3e24f7ab1
uuniversey <139521782+uuniversey@users.noreply.github.com>
2023-10-03 15:16:42 +0900

    [Silver IV] Title: 스택, Time: 72 ms, Memory: 34292 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+N = int(input())
+deq = deque()
+for _ in range(N):
+    order = sys.stdin.readline().split()
+    if order[0] == 'push':
+        deq.append(int(order[1]))
+    elif order[0] == 'pop':
+        if not deq:
+            print(-1)
+        else:
+            print(deq.pop())
+    elif order[0] == 'size':
+        print(len(deq))
+    elif order[0] == 'empty':
+        if not deq:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'top':
+        if not deq:
+            print(-1)
+        else:
+            print(deq[-1])
