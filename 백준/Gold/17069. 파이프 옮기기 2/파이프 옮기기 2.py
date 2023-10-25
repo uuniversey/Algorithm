2cd7b52ad72540c38f7724e938c7f0b7ac4e58b7
uuniversey <139521782+uuniversey@users.noreply.github.com>
2023-10-25 17:13:43 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
@@ -0,0 +1,18 @@
+N = int(input())
+home = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0, 0, 0] for _ in range(N)] for _ in range(N)]
+dp[0][1][0] = 1
+
+for i in range(N):
+    for j in range(1, N):
+        if j+1 < N and home[i][j+1] == 0:
+            dp[i][j+1][0] = dp[i][j][0] + dp[i][j][2]
+
+        if i+1 < N and home[i + 1][j] == 0:
+            dp[i+1][j][1] = dp[i][j][1] + dp[i][j][2]
+
+        if i+1 < N and j+1 < N and home[i+1][j] == 0 and home[i][j+1] == 0 and home[i+1][j+1] == 0:
+            dp[i+1][j+1][2] = dp[i][j][0] + dp[i][j][1] + dp[i][j][2]
+
+answer = sum(dp[N-1][N-1])
+print(answer)
