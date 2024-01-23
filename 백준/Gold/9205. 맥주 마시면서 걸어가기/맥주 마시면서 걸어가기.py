d62fc7c2e844eefa6a1330327c065e22100489a9
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-01-23 22:53:27 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 48 ms, Memory: 31132 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,31 @@
+import sys
+
+def dfs(s):
+    global ans
+    if s == n+1:
+        ans = 'happy'
+        return
+
+    vstd[s] = 1
+    mx, my = market[s]
+    for idx, val in enumerate(market):
+        x, y = val
+        if vstd[idx] == 0 and abs(x - mx) + abs(y - my) <= 1000:
+            vstd[idx] = 1
+            dfs(idx)
+
+
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    market = (
+                [list(map(int, input().split()))] +
+                [list(map(int, input().split())) for _ in range(n)] +
+                [list(map(int, input().split()))]
+              )
+
+    vstd = [0] * (n+2)
+    ans = 'sad'
+    dfs(0)
+    print(ans)
