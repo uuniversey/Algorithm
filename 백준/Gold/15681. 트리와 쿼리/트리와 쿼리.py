590ac70f3871cf99706d57f01fb7ab3293bd7023
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-02-04 20:48:36 +0900

    [Gold V] Title: 트리와 쿼리, Time: 448 ms, Memory: 66492 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+def countquery(x):
+    dp[x] = 1
+    for t in Tree[x]:
+        if dp[t] == 0:
+            countquery(t)
+            dp[x] += dp[t]
+
+
+input = sys.stdin.readline
+
+N, R, Q = map(int, input().split())
+Tree = [[] for _ in range(N+1)]
+dp = [0] * (N+1)
+for _ in range(N-1):
+    U, V = map(int, input().split())
+    Tree[U].append(V)
+    Tree[V].append(U)
+
+countquery(R)
+
+for _ in range(Q):
+    node = int(input())
+    print(dp[node])
