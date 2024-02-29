debe97bdf04c1d5150d7a95c1bdedf3a11a1cdf7
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-02-29 13:01:44 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,20 @@
+def comb(s, k, e):
+    if s == e:
+        ans.add(tuple(sets))
+    else:
+        for i in range(k, len(arr)):
+            sets[s] = arr[i]
+            comb(s+1, i, e)
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+sets = [0] * M
+ans = set()
+comb(0, 0, M)
+ans = list(ans)
+ans.sort()
+
+for a in ans:
+    print(*a)
