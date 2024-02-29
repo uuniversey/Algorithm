e11dfed42e14c6ae56831fc9f5dc8b24c70b6d3f
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-02-29 13:17:16 +0900

    [Silver II] Title: A → B, Time: 200 ms, Memory: 36260 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,18 @@
+import heapq
+
+A, B = map(int, input().split())
+hq = [(1, A)]
+flag = 1
+while hq:
+    num, calc = heapq.heappop(hq)
+    if calc > B:
+       continue
+    elif calc == B:
+        print(num)
+        flag = 0
+        break
+    heapq.heappush(hq, (num+1, calc * 2))
+    heapq.heappush(hq, (num+1, calc * 10 + 1))
+
+if flag:
+    print(-1)
