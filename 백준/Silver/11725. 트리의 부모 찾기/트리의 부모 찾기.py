a0a361e7c56e277be921f58a5e40ee0c09abb758
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-03-04 12:27:03 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4304 ms, Memory: 48828 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,23 @@
+def find(s):
+    for i in tree[s]:
+        if parents[i] == 0:
+            parents[i] = s
+            q.append(i)
+
+
+N = int(input())
+tree = [[] for _ in range(N+1)]
+parents = [0] * (N+1)
+
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+q = [1]
+parents[1] = 1
+while q:
+    num = q.pop()
+    find(num)
+
+[print(parents[i]) for i in range(2, N+1)]
