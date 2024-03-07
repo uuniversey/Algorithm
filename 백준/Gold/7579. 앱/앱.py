f6381eef64da5e272d0f4a4d25c07708fbd3ca72
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-03-08 08:52:30 +0900

    [Gold III] Title: 앱, Time: 788 ms, Memory: 48056 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+memory = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+
+dp = [[0] * (sum(cost)+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(0, sum(cost)+1):
+        if j >= cost[i-1]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-cost[i-1]]+memory[i-1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+res = sum(cost)+1
+for i in range(1, N+1):
+    for j in range(0, sum(cost)+1):
+        if dp[i][j] >= M:
+            res = min(j, res)
+
+print(res)
