23a5e6b4cc0a4dbdb71053e7e65f62027834e5da
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-03-15 09:30:07 +0900

    [Gold V] Title: 벼락치기, Time: 612 ms, Memory: 52796 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -0,0 +1,16 @@
+N, T = map(int, input().split())
+info = []
+for _ in range(N):
+    K, S = map(int, input().split())
+    info.append((K, S))
+
+dp = [[0] * (T+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(T+1):
+        if j >= info[i-1][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-info[i-1][0]] + info[i-1][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[-1][-1])
