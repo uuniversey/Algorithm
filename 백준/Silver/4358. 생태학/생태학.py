553d8ecc9b5083cce329830d4f3c816e62dadb07
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-03-22 09:30:13 +0900

    [Silver II] Title: 생태학, Time: 516 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -0,0 +1,17 @@
+import sys
+dic = {}
+amount = 0
+while True:
+    tree = sys.stdin.readline().rstrip()
+    if tree:
+        if not dic.get(tree):
+            dic[tree] = 1
+        else:
+            dic[tree] += 1
+        amount += 1
+    else:
+        break
+
+li = sorted(dic.items())
+for l in li:
+    print(l[0], f'{(l[1] / amount * 100):.4f}')
