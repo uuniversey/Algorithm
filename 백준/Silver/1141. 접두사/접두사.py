d4f7b4d666880a1e1e4f4f7328bf63f7d3ae0444
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-05-09 23:04:57 +0900

    [Silver I] Title: 접두사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1141. 접두사/접두사.py
+++ 백준/Silver/1141. 접두사/접두사.py
@@ -3,9 +3,9 @@ words = [input() for _ in range(N)]
 words.sort()
 res = []
 
-for i in range(N):
-    if i+1 < N and words[i+1].startswith(words[i]):
+for i in range(N-1):
+    if words[i+1].startswith(words[i]):
         continue
     res.append(words[i])
 
-print(len(res))
+print(len(res)+1)
