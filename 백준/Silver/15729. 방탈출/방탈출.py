a19e8f21d8ca529224d8ec512007f99489f5c940
uuniversey <139521782+uuniversey@users.noreply.github.com>
2024-06-26 14:23:20 +0900

    [Silver II] Title: 방탈출, Time: 500 ms, Memory: 49744 KB -BaekjoonHub

--- 백준/Silver/15729. 방탈출/방탈출.py
+++ 백준/Silver/15729. 방탈출/방탈출.py
@@ -1,26 +1,13 @@
-def toggle(n):
-    if n:
-        return 0
-    else:
-        return 1
-
-
 N = int(input())
 ans = list(map(int, input().split()))
-arr = [0] * N
+arr = [0] * (N+2)
 res = 0
 
 for i in range(N):
     if ans[i] != arr[i]:
         res += 1
-        arr[i] = toggle(arr[i])
-        try:
-            arr[i+1] = toggle(arr[i+1])
-        except:
-            continue
-        try:
-            arr[i+2] = toggle(arr[i+2])
-        except:
-            continue
+        arr[i] = 1-arr[i]
+        arr[i+1] = 1-arr[i+1]
+        arr[i+2] = 1-arr[i+2]
 
 print(res)
